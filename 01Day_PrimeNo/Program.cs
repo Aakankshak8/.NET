8d7a77cdf49fa3060aae9c57eeb2ffa4536a03c3
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-15 22:13:20 +0530

    15 Dec 25

---
+++ 01Day_PrimeNo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01Day_PrimeNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (IsPrime(n))
+                Console.WriteLine($"{n} is a Prime Number");
+            else
+                Console.WriteLine($"{n} is Not a Prime Number");
+
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ 02Day_Hello1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Day_Hello1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello...Welcome in my first .Net project");
+            Console.ReadLine();
+
+        }
+    }
+}
