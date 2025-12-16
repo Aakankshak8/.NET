34fd01185a62dbcf29aa1adcbef8e68a3e767311
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-17 00:14:53 +0530

    16 Dec 25

---
+++ 02Day_Database_Inheritance/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace _02Day_Database_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Choice: 1 -> MySqlServer, 2 -> SqlServer");
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+            DataFactory df = new DataFactory();
+            IDatabase dbs = df.GetDatabase(ch);
+
+            if (dbs == null)
+            {
+                Console.WriteLine("Invalid Database choice");
+                return;
+            }
+
+            Console.WriteLine("Enter the Option: 1 -> Insert, 2 -> Delete, 3 -> Update");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    dbs.Insert();
+                    break;
+
+                case 2:
+                    dbs.Delete();
+                    break;
+
+                case 3:
+                    dbs.Update();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter Valid choice");
+                    break;
+            }
+        }
+    }
+
+  
+    public interface IDatabase
+    {
+        void Insert();
+        void Update();
+        void Delete();
+    }
+
+
+    public class DataFactory
+    {
+        public IDatabase GetDatabase(int db)
+        {
+            switch (db)
+            {
+                case 1:
+                    return new MySqlServer();
+
+                case 2:
+                    return new SqlServer();
+
+                default:
+                    return null;
+            }
+        }
+    }
+
+  
+    public class SqlServer : IDatabase
+    {
+        public void Insert()
+        {
+            Console.WriteLine("Inside Insert Of SqlServer");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Inside Update Of SqlServer");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Inside Delete Of SqlServer");
+        }
+    }
+
+ 
+    public class MySqlServer : IDatabase
+    {
+        public void Insert()
+        {
+            Console.WriteLine("Inside Insert Of MySqlServer");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Inside Update Of MySqlServer");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Inside Delete Of MySqlServer");
+        }
+    }
+}
--- 02Day_Oop/Program.cs
+++ 02Day_Oop/Program.cs
@@ -5,44 +5,112 @@ namespace _02Day_Oop
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your choice of drink: 1-> Cold Drink  2->Soft Drink");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            #region MyRegion
+            //Console.WriteLine("Enter your choice of drink: 1-> Cold Drink  2->Soft Drink");
+            //int choice = Convert.ToInt32(Console.ReadLine());
 
-            switch (choice)
-            {
-                case 1:
-                    IDrink drink = new SoftDrink();
-                    drink.getDrink();
-                    break;
-                case 2:
-                    IDrink drink1=new HotDrink();
-                    drink1.getDrink();
-                    break;
-                default:
-                    Console.WriteLine("Enter valid Input");
-                    break;
+            //switch (choice)
+            //{
+            //    case 1:
+            //        IDrink drink = new SoftDrink();
+            //        drink.getDrink();
+            //        break;
+            //    case 2:
+            //        IDrink drink1=new HotDrink();
+            //        drink1.getDrink();
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter valid Input");
+            //        break;
 
-            }
+            //}
+
+            #endregion
+
+            MyMath myMath = new MyMath();
+            myMath.Add(6, 3);
+            myMath.Mult(6, 3);
+            myMath.log("Hello");
+
+            myMath.Div(6, 3);
+
+
 
         }
 
-        public interface IDrink {
-            void getDrink();
-        }
-        public class SoftDrink : IDrink {
-            public void getDrink()
-            {
+        #region MyRegion
+        //public interface IDrink
+        //{
+        //    void getDrink();
+        //}
+        //public class SoftDrink : IDrink
+        //{
+        //    public void getDrink()
+        //    {
 
-                Console.WriteLine("Enjoy your cold drink");
+        //        Console.WriteLine("Enjoy your cold drink");
+        //    }
+        //}
+        //public class HotDrink : IDrink
+        //{
+        //    public void getDrink()
+        //    {
+        //        Console.WriteLine("Enjoy your soft drink");
+        //    } 
+
+        //}
+        #endregion
+
+        public interface IX
+        {
+            public void Add(int a, int b)
+            {
+                Console.WriteLine(a + ", " + b);
+            }
+            public void Sub(int a, int b)
+            {
+                Console.WriteLine(a - b);
             }
         }
-        public class HotDrink: IDrink {
-            public void getDrink()
+        public interface IY
+        {
+            public void Add(int a, int b);
+            public void Mult(int a, int b);
+
+        }
+        public interface IDemo
+        {
+            public void Div(int a, int b);
+
+            public void log(String msg);
+
+        }
+        public class MyMath : IY, IDemo
+
+        {
+            public void Div(int a, int b)
             {
-                Console.WriteLine("Enjoy your soft drink");
+                Console.WriteLine(a / b);
             }
+            public void log(String msg)
+            {
+                Console.WriteLine(msg);
+
+            }
+
+            public void Add(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+            public void Mult(int a, int b)
+            {
+                Console.WriteLine(a * b);
+            }
+
+
         }
 
 
