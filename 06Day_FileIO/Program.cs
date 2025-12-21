6178bf485d71305c02ca4129157cbb2110d8f829
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-21 23:33:08 +0530

    20 Dec 2025

---
+++ 06Day_FileIO/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _06Day_FileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "D:\\IET\\.NET\\Solution1\\06Day_FileIO\\Data\\Info.txt";
+
+            #region CreateorWrite
+            //FileStream fs = null;
+            //if (File.Exists(path))
+            //{
+            //    fs = new FileStream(path, FileMode.Append, FileAccess.Write);
+            //}
+            //else
+            //{
+            //    fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            //}
+
+
+            //StreamWriter writer = new StreamWriter(fs);
+            //writer.WriteLine("HELLO FROM INPUT OUTPUT OPERATION");
+            //writer.Flush();
+            //writer.Close();
+            //fs.Close();
+            //Console.WriteLine("Done"); 
+            #endregion
+
+
+            //FileStream fs = null;
+            //if (File.Exists(FilePath))
+            //{
+            //    fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File not found");
+            //}
+
+            //StreamReader reader = new StreamReader(fs);
+            //String Content = reader.ReadToEnd();
+            //reader.Close();
+            //fs.Close();
+            //Console.WriteLine(Content); 
+
+            FileStream fs = null;
+            if (File.Exists(path))
+            {
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+
+
+            StreamReader Reader= new StreamReader(fs);
+            String content = Reader.ReadToEnd();
+            Reader.Close();
+            fs.Close();
+
+            Console.WriteLine(content); 
+
+
+        }
+    }
+}
