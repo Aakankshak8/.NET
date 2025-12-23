acefe89c26bc80a5558468b8567c100a0df1f5a1
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-23 23:31:24 +0530

    22 Dec 2025

---
+++ 07Day_MyLib/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07Day_MyLib
+{
+    // Define custom Table attribute
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        public string TableName { get; set; }
+    }
+
+    // Define custom Column attribute
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnAttribute : Attribute
+    {
+        public string ColumnName { get; set; }
+        public string ColumnType { get; set; }
+    }
+
+    [Table(TableName = "Employee")]
+    public class Emp
+    {
+        private string _Name;
+        private string _Address;
+
+        [Column(ColumnName = "EId", ColumnType = "int")]
+        public int Id { get; set; }
+
+        [Column(ColumnName = "EName", ColumnType = "varchar(50)")]
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        [Column(ColumnName = "EAddress", ColumnType = "varchar(50)")]
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+    }
+}
---
+++ 07Day_MyORM/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Reflection;
+
+
+namespace _07Day_MyORM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string asmPath = "D:\\IET\\.NET\\Solution1\\07Day_MyLib\\bin\\Debug\\07Day_MyLib.dll";
+
+            Assembly asm = Assembly.LoadFrom(asmPath);
+            Type[] allTypes = asm.GetTypes();
+
+            foreach (Type type in allTypes)
+            {
+                string createTableQuery = "CREATE TABLE ";
+
+                Attribute[] allAttributes = type.GetCustomAttributes().ToArray();
+
+                foreach (Attribute attr in allAttributes)
+                {
+                    if (attr is TableAttribute tableAttr)
+                    {
+                        createTableQuery += tableAttr.TableName + " ( ";
+                    }
+                }
+
+                PropertyInfo[] allProperties = type.GetProperties();
+
+                foreach (PropertyInfo prop in allProperties)
+                {
+                    Attribute[] propAttr = prop.GetCustomAttributes().ToArray();
+
+                    foreach (Attribute pAttr in propAttr)
+                    {
+                        if (pAttr is ColumnAttribute col)
+                        {
+                            createTableQuery += col.ColumnName + " " + col.ColumnType + ",";
+                        }
+                    }
+                }
+
+                createTableQuery = createTableQuery.TrimEnd(',') + " )";
+                Console.WriteLine(createTableQuery);
+
+                string filePath = "D:\\IET\\.NET\\Solution1\\07Day_MyORM\\SQLQuery\\CreateQuery.sql";
+                File.AppendAllText(filePath, createTableQuery + Environment.NewLine);
+
+                Console.WriteLine("Done.");
+            }
+        }
+    }
+}
