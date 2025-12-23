35431775d42a94e1b339b100e52d5cb7c9429d47
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-23 11:18:17 +0530

    23 Dec 2025

---
+++ 07Day_PartialClass/File1/CMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Day_PartialClass
+{
+    internal partial class CMath
+    {
+
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+    }
+}
---
+++ 07Day_PartialClass/File2/CMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Day_PartialClass
+{
+    internal partial class CMath
+    {
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine(x*y);
+        }
+    }
+}
