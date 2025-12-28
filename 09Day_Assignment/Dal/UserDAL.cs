db969db585c7ead072aa6719cd0b9165be2c1da2
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-12-28 23:30:20 +0530

    Assignment

---
+++ 09Day_Assignment/Dal/UserDAL.cs
@@ -0,0 +1,53 @@
+﻿using _09Day_Assignment.Model;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09Day_Assignment.Dal
+{
+    internal class UserDAL
+    {
+        private string connectionString =
+        "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aakanksha;Integrated Security=True";
+
+        internal int CreateUser(User userr)
+        {
+            using SqlConnection con = new SqlConnection(connectionString);
+            string query =
+                "INSERT INTO Userr (Email, Password) VALUES (@email, @password)";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@email", userr.Email);
+            cmd.Parameters.AddWithValue("@password", userr.Password);
+
+            con.Open();
+            int rows = cmd.ExecuteNonQuery();
+            return rows;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+
+            using SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(
+                "SELECT Email, Password FROM Userr", con);
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                users.Add(new User
+                {
+                    Email = reader["Email"].ToString(),
+                    Password = reader["Password"].ToString()
+                });
+            }
+            return users;
+        }
+    }
+}
---
+++ 09Day_Assignment/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;          
+
+
+using _09Day_Assignment.Dal;
+using _09Day_Assignment.Model;
+
+namespace _09Day_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDAL dAL = new UserDAL();
+            while (true)
+            {
+                Console.WriteLine("1.Login");
+                Console.WriteLine("2.Create User");
+                Console.WriteLine("Enter Choice : ");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Email: ");
+                        String email = Console.ReadLine();
+                        Console.WriteLine("Enter Password: ");
+                        String password = Console.ReadLine();
+
+                        var users = dAL.GetAllUsers();
+
+                        var validUser = users
+                                       .FirstOrDefault(u => u.Email == email && u.Password == password);
+
+
+                        if (validUser != null)
+                            Console.WriteLine(" Login successful");
+                        else
+                            Console.WriteLine(" Invalid username or password");
+                        break;
+
+                    case 2: 
+                        User user = new User();
+
+                        Console.Write("Enter Email: ");
+                        user.Email = Console.ReadLine();
+
+                        
+                        if (!user.Email.Contains("@gmail.com"))
+                        {
+                            Console.WriteLine("Invalid email! Email must be a gmail address.");
+                            break;  
+                        }
+
+                        Console.Write("Enter Password: ");
+                        user.Password = Console.ReadLine();
+
+                        int rows = dAL.CreateUser(user);
+                        Console.WriteLine(rows > 0
+                            ? "User created successfully"
+                            : "Error creating user");
+                        break;
+
+
+                }
+            }
+        }
+    }
+}
